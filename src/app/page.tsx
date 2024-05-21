df9aaf3cb51480ef10f152163f0539c8c5e8e8f0
Radek Sienkiewicz <mail@velvetshark.com>
2024-05-21 02:48:53 +0200

    Update site title and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ const HomePage = () => {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="max-w-8xl container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <div className="max-w-8xl container flex flex-col items-center justify-center gap-6 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           EIP Directory
         </h1>
