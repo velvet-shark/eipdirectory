88361efaac89b8ef23fd8b4cd289595a43d80c0b
Radek Sienkiewicz <mail@velvetshark.com>
2024-05-21 02:30:33 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,127 @@
+"use client";
+import { Fragment, useEffect, useState } from "react";
+
+type EIP = {
+  EIP: string;
+  Number: string;
+  Category: string;
+  Title: string;
+  "Custom name": string;
+  Status: string;
+  Created: string;
+  Summary: string;
+  "Why is it important?": string;
+  Abstract: string;
+  Motivation: string;
+  Description: string;
+  "Official link": string;
+  "GitHub link": string;
+  Links: string;
+  Requires: string;
+  Body: string;
+};
+
+const HomePage = () => {
+  const [eips, setEips] = useState<EIP[]>([]);
+
+  useEffect(() => {
+    fetch("/eips.json")
+      .then((response) => response.json())
+      .then((data: EIP[]) => setEips(data))
+      .catch((error) => console.error("Error fetching EIPs:", error));
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="max-w-8xl container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          EIP Directory
+        </h1>
+        <div className="max-w-8xl container mx-auto px-4 py-8">
+          <div className="overflow-x-auto">
+            <table className="min-w-full border border-gray-300">
+              <thead>
+                <tr className="bg-gray-100">
+                  <th className="border-b px-4 py-2 text-left align-top text-gray-800">
+                    Name
+                  </th>
+                  <th className="border-b px-4 py-2 text-left align-top text-gray-800">
+                    Description
+                  </th>
+                  <th className="border-b px-4 py-2 text-left align-top text-gray-800">
+                    Why is it important?
+                  </th>
+                  <th className="border-b px-4 py-2 text-left align-top text-gray-800">
+                    Status
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {eips.map((eip) => (
+                  <Fragment key={eip.Number}>
+                    <tr className="border-b border-gray-100 bg-white ">
+                      <td className="px-4 py-2 align-top font-semibold text-gray-800">
+                        {eip.EIP}: {eip.Title}
+                      </td>
+                      <td className="px-4 py-2 align-top text-gray-800">
+                        {eip.Summary}
+                      </td>
+                      <td className="px-4 py-2 align-top text-gray-800">
+                        {eip["Why is it important?"]}
+                      </td>
+                      <td className="px-4 py-2 align-top text-gray-800">
+                        {eip.Status}
+                      </td>
+                    </tr>
+                    <tr className="border-b-2  border-purple-500 border-opacity-50 bg-white py-4">
+                      <td className="border-b px-4 py-2 align-top text-gray-800">
+                        <a
+                          href={eip["Official link"]}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-blue-600 hover:underline"
+                        >
+                          Official Link
+                        </a>
+                        <br />
+                        <a
+                          href={eip["GitHub link"]}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-blue-600 hover:underline"
+                        >
+                          GitHub Link
+                        </a>
+                      </td>
+                      <td
+                        colSpan={2}
+                        className="border-b px-4 py-2 align-top text-gray-800"
+                      >
+                        <details>
+                          <summary className="cursor-pointer text-blue-600">
+                            Abstract
+                          </summary>
+                          <div className="mt-2">{eip.Abstract}</div>
+                        </details>
+                        <details>
+                          <summary className="cursor-pointer text-blue-600">
+                            Motivation
+                          </summary>
+                          <div className="mt-2">{eip.Motivation}</div>
+                        </details>
+                      </td>
+                      <td className="border-b px-4 py-2 align-top text-gray-800">
+                        Created: {eip.Created}
+                      </td>
+                    </tr>
+                  </Fragment>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default HomePage;
---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      },
+      fontSize: {
+        xs: "0.75rem",
+        sm: "0.875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+      },
+    },
+    screens: {
+      sm: "100%",
+      md: "100%",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+      "4xl": "2560px",
+    },
+  },
+  plugins: [],
+} satisfies Config;
